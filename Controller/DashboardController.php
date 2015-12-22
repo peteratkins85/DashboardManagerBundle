ddb45d4f3a3f9e2fcd792afc0b2709ee15149df8
Peter Atkins <peteratkins@Peters-iMac.local>
2015-12-22 19:33:50 +0000

    update to Symfony 3

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -2,16 +2,34 @@
 
 namespace Cms\DashboardManagerBundle\Controller;
 
+use Cms\CoreBundle\Controller\CoreController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 
-class DashboardController extends Controller
+class DashboardController extends CoreController
 {
+
+      private $pageName = 'Dashboard';
+
+
+    public function __construct()
+    {
+
+
+    }
+
     public function indexAction()
     {
-        return $this->render('DashboardManagerBundle:Dashboard:index.html.twig', array('pageName' => 'Dashboard'));
+
+        $session = new Session();
+
+        return $this->render('DashboardManagerBundle:Dashboard:index.html.twig',
+            array(
+                'pageName' => $this->pageName
+            )
+        );
     }
 }
 
